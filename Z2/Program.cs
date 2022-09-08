066e4b4cb03998d5499b21c3a6809c0a57f13143
Vika <mandarinkavikabakalova@gmail.com>
2022-09-08 23:18:54 +0400

    Задание2

---
+++ Z2/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Максимальное число:");
+Console.Write(Math.Max(num1,num2));
